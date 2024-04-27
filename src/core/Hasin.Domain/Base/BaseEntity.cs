d83e52535dd5b3d717e96c6ba9e411f7a841bc36
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2024-04-27 09:18:25 +0330

    remove projects

--- src/core/Hasin.Domain/Base/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hasin.Domain.Base
-{
-    public abstract class BaseEntity<T>
-    {
-        public required T Id { get; set; }
-    }
-
-    public abstract class BaseEntity : BaseEntity<int>;
-}
